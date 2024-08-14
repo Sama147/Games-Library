1a643a5f815e79c403d09f1885689b1701bd8bf8
samaashraf <samaashraf147@gmail.com>
2024-08-14 10:37:25 +0300

    Adding guest and admin files plus more imrovments on other files

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d71ecee6bc758484cc84897a77403e722d8ebd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0091c610fc58592e54ab3245c5aa518e3dbb79f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
